d23e6203ee4ed4c98374d5ad4ed32a0df9b91540
mmmahh <1600325372@qq.com>
2020-07-07 16:10:53 +0800

    add: insert insertFixup

--- src/algorithm/datastructure/RBTree.java
+++ src/algorithm/datastructure/RBTree.java
@@ -1,7 +1,7 @@
 package algorithm.datastructure;
 
 public class RBTree {
-	public enum Color {RED,BLOCK};
+	public enum Color {RED,BLACK};
 	public final RBNode nil;
 	
 	class RBNode 
@@ -10,15 +10,17 @@ public class RBTree {
 		RBNode left;
 		RBNode right;
 		RBNode parent;
+		int data;
 		public RBNode()
 		{
 			this(0);
 		}
 		public RBNode(int data) {
 			this.color = Color.RED;
-			this.left = nil;
-			this.right = nil;
-			this.parent = nil;
+			this.left = null;
+			this.right = null;
+			this.parent = null;
+			this.data = data;
 		}
 	}
 	
@@ -27,7 +29,7 @@ public class RBTree {
 	public RBTree()
 	{
 		this.nil = new RBNode();
-		nil.color = Color.BLOCK;
+		this.nil.color = Color.BLACK;
 		this.root = this.nil;
 	}
 	
@@ -78,6 +80,90 @@ public class RBTree {
 		}
 		y.right = x;
 		x.parent = y;
+	}
+	public void insert(RBNode z)
+	{
+		RBNode y = this.nil;
+		RBNode x = this.root;
+		while(x!=this.nil)
+		{
+			y = x;
+			if(z.data<x.data)
+			{
+				x = x.left;
+			}
+			else
+			{
+				x = x.right;
+			}
+		}
+		z.parent = y;
+		if(y==this.nil)
+		{
+			this.root = z;
+		}
+		else if(z.data<y.data)
+		{
+			y.left = z;
+		}
+		else
+		{
+			y.right = z;
+		}
+		z.left = this.nil;
+		z.right = this.nil;
+		z.color = Color.RED;
+		insertFixup(z);
+	}
+	private void insertFixup(RBNode z)
+	{
+		while(z.parent.color==Color.RED)
+		{
+			if(z.parent==z.parent.parent.left)
+			{
+				RBNode y = z.parent.parent.right;
+				if(y.color==Color.RED)
+				{
+					z.parent.color = Color.BLACK;
+					z.parent.color = Color.BLACK;
+					z.parent.parent.color = Color.RED;
+					z = z.parent.parent;
+				}
+				else
+				{
+					if(z==z.parent.right)
+					{
+						z = z.parent;
+						this.leftRotate(z);
+					}
+					z.parent.color = Color.BLACK;
+					z.parent.parent.color = Color.RED;
+					this.rightRotate(z.parent.parent);
+				}
+			}
+			else
+			{
+				RBNode y = z.parent.parent.left;
+				if(y.color==Color.RED)
+				{
+					z.parent.color = Color.BLACK;
+					z.parent.color = Color.BLACK;
+					z.parent.parent.color = Color.RED;
+					z = z.parent.parent;
+				}
+				else
+				{
+					if(z==z.parent.left)
+					{
+						z = z.parent;
+						this.rightRotate(z);
+					}
+					z.parent.color = Color.BLACK;
+					z.parent.parent.color = Color.RED;
+					this.leftRotate(z.parent.parent);
+				}
+			}
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
